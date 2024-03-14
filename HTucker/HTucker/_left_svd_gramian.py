4640532741e7b174058ca788286a148b77b6a0c0
Benedikt Schröter <benedikt.j.schroeter@gmail.com>
2024-03-14 12:17:06 +0100

    ref: changed eigh in left_svd_gramian to svd

--- HTucker/HTucker/_left_svd_gramian.py
+++ HTucker/HTucker/_left_svd_gramian.py
@@ -16,7 +16,7 @@ def left_svd_gramian(x: torch.Tensor):
                                         auf die Singulaerwerte in absteigender Reihenfolge sortiert.
     """
     # Spektralzerlegung
-    eig_val, Q = torch.linalg.eigh(x)
+    Q, eig_val, _ = torch.linalg.svd(x, full_matrices=False)
     # Singulaerwerte entsprechen den Quadratwurzeln der Eigenwertbetraege
     s_val = torch.sqrt(torch.abs(eig_val))
     # Index zur absteigenden Sortierung der Singulaerwerte
