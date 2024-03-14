db9ed03ecd3cc0d7a995cf28d0fd89772ee0e6a4
Benedikt Schröter <benedikt.j.schroeter@gmail.com>
2024-03-14 15:04:04 +0100

    feat: dynamically chooses algorithm based on location of the tensor (cpu vs. gpu)

--- HTucker/HTucker/_left_svd_gramian.py
+++ HTucker/HTucker/_left_svd_gramian.py
@@ -1,11 +1,12 @@
 import torch
+from scipy.linalg import svd
 
 
 def left_svd_gramian(x: torch.Tensor):
     """
-    Berechnet die linken Singulaervektoren samt Singulaerwerte der Matrix 'x' und gibt diese absteigend sortiert
+    Berechnet die linken Singulaervektoren samt Singulaerwerte der Matrix 'v' und gibt diese absteigend sortiert
     zurueck.
-    Die Berechnungen beruhen auf der Annahme, dass 'x' eine reduzierte Gram'sche Matrix der Form x = v.T @ v ist.
+    Die Berechnungen beruhen auf der Annahme, dass 'v' eine reduzierte Gram'sche Matrix der Form v= x.T @ x ist.
     ______________________________________________________________________
     Parameter:
     - x 2D torch.Tensor
@@ -15,10 +16,16 @@ def left_svd_gramian(x: torch.Tensor):
                                         Eintrag den zugehoerigen Singulaerwerten entspricht. Das Tupel ist bezogen
                                         auf die Singulaerwerte in absteigender Reihenfolge sortiert.
     """
-    # Spektralzerlegung
-    Q, eig_val, _ = torch.linalg.svd((x+x)/2, full_matrices=False)
-    # Singulaerwerte entsprechen den Quadratwurzeln der Eigenwertbetraege
-    s_val = torch.sqrt(torch.abs(eig_val))
-    # Index zur absteigenden Sortierung der Singulaerwerte
-    desc_idc = torch.argsort(s_val, dim=0, descending=True)
-    return Q[:, desc_idc], s_val[desc_idc]
+
+    if x.is_cuda:
+        ## Spektralzerlegung
+        eig_val, Q = torch.linalg.eigh(x)
+        # Singulaerwerte entsprechen den Quadratwurzeln der Eigenwertbetraege
+        s_val = torch.sqrt(torch.abs(eig_val))
+        # Index zur absteigenden Sortierung der Singulaerwerte
+        desc_idc = torch.argsort(s_val, dim=0, descending=True)
+        return Q[:, desc_idc], s_val[desc_idc]
+    else:
+        xnpy = x.numpy()
+        Q, eig_val, _ = svd(xnpy, full_matrices=False, lapack_driver="gesvd")
+    return torch.Tensor(Q), torch.sqrt(torch.Tensor(eig_val))
