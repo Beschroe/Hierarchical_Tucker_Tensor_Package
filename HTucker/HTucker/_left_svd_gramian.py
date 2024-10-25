f1ad42395b5d4bced4d316252a40a97e973448fa
Benedikt S. <benedikt.j.schroeter@gmail.com>
2024-10-25 12:06:16 +0200

    bugfix: fixed bug in left_svd_gramian

--- HTucker/HTucker/_left_svd_gramian.py
+++ HTucker/HTucker/_left_svd_gramian.py
@@ -1,12 +1,10 @@
 import torch
-from scipy.linalg import svd
 
 
 def left_svd_gramian(x: torch.Tensor):
     """
-    Berechnet die linken Singulaervektoren samt Singulaerwerte der Matrix 'v' und gibt diese absteigend sortiert
-    zurueck.
-    Die Berechnungen beruhen auf der Annahme, dass 'v' eine reduzierte Gram'sche Matrix der Form v= x.T @ x ist.
+    Berechnet die linken Singulaervektoren samt Singulaerwerte einer Matrix
+    v ueber ihre Gram-Matrix x=v@v.T
     ______________________________________________________________________
     Parameter:
     - x 2D torch.Tensor
@@ -17,15 +15,12 @@ def left_svd_gramian(x: torch.Tensor):
                                         auf die Singulaerwerte in absteigender Reihenfolge sortiert.
     """
 
-    if x.is_cuda:
-        ## Spektralzerlegung
-        eig_val, Q = torch.linalg.eigh(x)
-        # Singulaerwerte entsprechen den Quadratwurzeln der Eigenwertbetraege
-        s_val = torch.sqrt(torch.abs(eig_val))
-        # Index zur absteigenden Sortierung der Singulaerwerte
-        desc_idc = torch.argsort(s_val, dim=0, descending=True)
-        return Q[:, desc_idc], s_val[desc_idc]
-    else:
-        xnpy = x.numpy()
-        Q, eig_val, _ = svd(xnpy, full_matrices=False, lapack_driver="gesvd")
-    return torch.Tensor(Q), torch.sqrt(torch.Tensor(eig_val))
+
+    ## Spektralzerlegung
+    eig_val, Q = torch.linalg.eigh(x)
+    # Singulaerwerte entsprechen den Quadratwurzeln der Eigenwertbetraege
+    s_val = torch.sqrt(torch.abs(eig_val))
+    # Index zur absteigenden Sortierung der Singulaerwerte
+    desc_idc = torch.argsort(s_val, dim=0, descending=True)
+    return Q[:, desc_idc], s_val[desc_idc]
+
